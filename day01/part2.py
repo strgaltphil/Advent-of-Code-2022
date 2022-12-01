51f3e8f666411f698b0d575e14c213b1e6a15018
Philipp Spiertz <p.spiertz@comed-com.de>
2022-12-01 15:00:32 +0100

    Refactoring

--- day01/part2.py
+++ day01/part2.py
@@ -3,4 +3,4 @@ from itertools import groupby
 with open('input.txt', 'r') as file:
     data = [line for line in file.read().split('\n')]
 
-print(sum(sorted([sum(list(int(i) for i in g)) for k, g in groupby(data, key=bool) if k], reverse=True)[:3]))
+print(sum(sorted([sum(list(int(i) for i in g)) for k, g in groupby(data, key=bool) if k])[-3:]))
