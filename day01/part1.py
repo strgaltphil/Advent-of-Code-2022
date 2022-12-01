e76fd64226eaf967c23177fb32df3464f0e917cd
Philipp Spiertz <p.spiertz@comed-com.de>
2022-12-01 15:54:24 +0100

    Refactoring

--- day01/part1.py
+++ day01/part1.py
@@ -3,4 +3,4 @@ from itertools import groupby
 with open('input.txt', 'r') as file:
     data = [line for line in file.read().split('\n')]
 
-print(max([sum(list(int(i) for i in g)) for k, g in groupby(data, key=bool) if k]))
+print(max([sum(map(int, g)) for k, g in groupby(data, key=bool) if k]))
