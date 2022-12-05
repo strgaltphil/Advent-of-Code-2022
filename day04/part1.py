848e06c31d314cde2c8027d6fe31c5160770f588
Philipp Spiertz <p.spiertz@comed-com.de>
2022-12-05 11:18:27 +0100

    Day 4

---
+++ day04/part1.py
@@ -0,0 +1,14 @@
+with open('input.txt', 'r') as file:
+    data = [[list(map(int, l.split('-'))) for l in line.split(',')] for line in file.read().split('\n')]
+
+overlapping_spaces = 0
+
+for d in data:
+    first_elve = set(range(d[0][0], d[0][1] + 1))
+    second_elve = set(range(d[1][0], d[1][1] + 1))
+    intersection = first_elve & second_elve
+
+    if intersection == first_elve or intersection == second_elve:
+        overlapping_spaces += 1
+
+print(overlapping_spaces)
